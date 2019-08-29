1a9acbc294ffbdd26814bfdb1bc3adbfef97b750
Daysi <daysi200992qgd@gmail.com>
2019-08-29 05:49:09 +0400

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="container">
+   
+    <Cabecera></Cabecera>
+    <lista></lista>
+  </div>
+</template>
+
+<script>
+import Cabecera from './components/Cabecera.vue'
+import Lista from './components/Lista'
+
+export default {
+  name: 'app',
+  components: { 
+    Cabecera,
+    Lista
+  }
+}
+</script>
+
+
---
+++ src/store.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+   frutas:[
+     {nombre:'manzana', cantidad: 0},
+     {nombre:'pera', cantidad:0},
+     {nombre:'tomate', cantidad:0},
+    ]
+  },
+  mutations: {
+  aumentar(state,index){
+    state.frutas[index].cantidad ++ 
+   },
+
+  reiniciar(state){
+    state.frutas.forEach(Element =>{
+      Element.cantidad=0;
+    })
+  }
+  },
+  actions: {
+
+  }
+})
